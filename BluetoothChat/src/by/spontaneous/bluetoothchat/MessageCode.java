53f706dfae7b456044b7256ad1e323ad27342720
Artemios Jonash <eai_bat@tut.by>
2016-08-21 18:30:34 +0300

    Предварительное состояние проекта
    Рабочий проект в том виде, в котором он был готов на середину августа
    2016 (без использования системы контроля версий)

---
+++ BluetoothChat/src/by/spontaneous/bluetoothchat/MessageCode.java
@@ -0,0 +1,30 @@
+package by.spontaneous.bluetoothchat;
+
+enum MessageCode
+{
+	UNKNOWN(0), MESSAGE_READ(1);
+
+	private final int id;
+
+	MessageCode(int code)
+	{
+		id = code;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public static MessageCode fromId(int code)
+	{
+		MessageCode[] list = MessageCode.values();
+
+		if (code >= 0 && code < list.length)
+		{
+			return list[code];
+		}
+
+		return MessageCode.UNKNOWN;
+	}
+}
